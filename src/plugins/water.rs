1e02b367bb8288b55cc6e44b6d03227121da3f03
Alex Hill <achill113@gmail.com>
2024-03-27 20:08:51 -0400

    feat: add bevy_water back in

--- src/plugins/water.rs
+++ src/plugins/water.rs
@@ -1,4 +1,5 @@
 use bevy::{pbr::NotShadowCaster, prelude::*};
+use bevy_water::{material::{StandardWaterMaterial, WaterMaterial}, WaterPlugin as BevyWaterPlugin, *};
 
 use super::tile::{TileSettings, GRID_SIZE};
 
@@ -7,15 +8,20 @@ pub struct WaterPlugin;
 impl Plugin for WaterPlugin {
     fn build(&self, app: &mut App) {
         app
+            .insert_resource(WaterSettings {
+                ..default()
+            })
+            .add_plugins(BevyWaterPlugin)
             .add_systems(Startup, setup);
     }
 }
 
 fn setup(
     mut commands: Commands,
+    water_settings: Res<WaterSettings>,
     tile_settings: Res<TileSettings>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut materials: ResMut<Assets<StandardWaterMaterial>>,
 ) {
     let size = tile_settings.tile_size * GRID_SIZE as f32;
 
@@ -23,10 +29,16 @@ fn setup(
         Plane3d::default().mesh().size(size, size)
     );
 
-    let material = materials.add(StandardMaterial {
-        base_color: Color::rgba_u8(114, 162, 208, 100),
-        alpha_mode: AlphaMode::Add,
-        ..default()
+    let material = materials.add(StandardWaterMaterial {
+        base: default(),
+        extension: WaterMaterial {
+            amplitude: water_settings.amplitude,
+            coord_scale: Vec2::new(256.0, 256.0),
+            ..default()
+        }
+        // base_color: Color::rgba_u8(114, 162, 208, 100),
+        // alpha_mode: AlphaMode::Add,
+        // ..default()
     });
 
     commands.spawn((
